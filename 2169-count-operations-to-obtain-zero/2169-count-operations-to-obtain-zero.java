6d10b45ea3bb494e9fc8413de343de299ee63fb0
vartularora45 <138357352+vartularora45@users.noreply.github.com>
2025-11-09 09:09:06 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.68%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int c = 0;
+        while(num1 != 0 && num2 != 0) {
+            if(num1 >= num2) {
+                c += num1 / num2;  // kitni baar subtract hoga ek hi step me
+                num1 = num1 % num2;
+            } else {
+                c += num2 / num1;
+                num2 = num2 % num1;
+            }
+        }
+        return c;
+    }
+}
