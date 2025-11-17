57ca8daf51aa220c64eec3500796912edf923bc1
vartularora45 <138357352+vartularora45@users.noreply.github.com>
2025-11-17 10:23:52 +0530

    Time: 4 ms (96.36%), Space: 52.3 MB (24.46%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> grayCode(int n) {
+        int size = 1 << n;
+        List<Integer> result = new ArrayList<>(size);
+        
+        for (int i = 0; i < size; i++) {
+            result.add(i ^ (i >> 1)); 
+        }
+        
+        return result;
+    }
+}
