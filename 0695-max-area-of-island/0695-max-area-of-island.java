cd2d2f2b9d38472d5f069fc4df4aef4877b27368
vartularora45 <138357352+vartularora45@users.noreply.github.com>
2025-11-25 13:43:21 +0530

    Time: 1 ms (100%), Space: 46.4 MB (26.06%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -18,7 +18,7 @@ class Solution {
             return 0;
         }
         if(grid[i][j] == 0){
-            return 0; 
+            return 0;
         }
         grid[i][j] = 0;
         return 1+dfs(grid,i+1,j)+dfs(grid,i-1,j)+dfs(grid,i,j+1)+dfs(grid,i,j-1);
