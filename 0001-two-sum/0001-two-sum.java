6b3d1dd27acd37817d11ee08e01cc1363e05ca93
Vartul Arora <138357352+vartularora45@users.noreply.github.com>
2026-03-09 19:43:12 +0530

    Time: 449 ms (6.63%), Space: 46.6 MB (95.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -6,9 +6,10 @@ class Solution {
             l.add(nums[i]);
         }
         for(int i=0;i<nums.length;i++){
-            if(l.contains(target - nums[i])){
+            if(l.contains(target - nums[i]) && l.indexOf(target - nums[i]) != i){
                 res[0] = i;
                 res[1] = l.indexOf(target-nums[i]);
+                
             }
         }    
         return res;
