fac38c4c3732ec6ff7783c3e719be982d50aaa2f
Vartul Arora <138357352+vartularora45@users.noreply.github.com>
2026-03-09 20:27:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,13 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for(int i: nums) {
-            map.put(i, map.getOrDefault(i, 0)+1);
-            if(map.get(i)>n/2) return i;
-        }
-
-        return -1;
+       Arrays.sort(nums);
+       int n = nums.length;
+       return nums[n/2];
     }
 }
