5793c99169ee754226fae00c636d34753204d37f
Vartul Arora <138357352+vartularora45@users.noreply.github.com>
2026-04-20 11:59:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
@@ -4,7 +4,7 @@ class Solution {
         int mx = Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
             
-            for(int j=0;j<n-1;j++){
+            for(int j=i+1;j<n;j++){
                 if(colors[i] != colors[j]){
                     mx = Math.max(mx,Math.abs(j-i));
 
